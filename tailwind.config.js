651ae72c4acf9d27021021cc79b461acb6cf20f5
eduardo1403 <eduardo1403hm@gmail.com>
2023-11-05 22:41:51 -0600

    Configuración de entorno

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.css",
+    "./resources/**/*.vue",
+    "./resources/**/*.jpg",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
